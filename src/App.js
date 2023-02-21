a86eddc0108743e5b455c88d552e7e632c901ee1
Swapnil Shah <swapnilshah5889@gmail.com>
2023-02-20 21:29:09 -0500

    temporary commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
+import React from 'react';
+import { ReactDOM } from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+import Lottie from "lottie-react";
+import responsiveFile from "./data/responsive-lottie.json";
 
 function App() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: responsiveFile,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        {/* Button */}
+        <a className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React With Swapnil
         </a>
+        <Lottie
+          options={defaultOptions}>
+        </Lottie>
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+
       </header>
+
     </div>
   );
 }
